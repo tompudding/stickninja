ea94f7f96d7ef1fac97a429a7051803b632580d2
Tom Gooding <tom.pudding@gmail.com>
2014-12-06 10:33:30 +0000

    Initial commit from skeleton

---
+++ drawing/__init__.py
@@ -0,0 +1,3 @@
+from quads  import Quad,QuadBuffer,QuadBorder,LineBuffer,Line
+from opengl import Init,NewFrame,DrawAll,InitDrawing,DrawNoTexture,ResetState,Scale,Translate,LineWidth
+import texture,opengl,sprite
